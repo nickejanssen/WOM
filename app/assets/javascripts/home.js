8cb03a11073024123cbef9512cdeeb99d98c4d8b
nickejanssen <nickejanssen@gmail.com>
2016-02-29 15:01:58 -0500

    Initial Commit
    New Version of Word of Mouth Website

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,41 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+var readURL = function(input, preview) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $(preview).parent().removeClass('hidden');
+      $(preview).attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+};
+
+$(document).ready(function() {
+
+  var preview = "#img-preview > img";
+
+  $("#post-attachment").click(function() {
+    $("#post_attachment").click();
+  });
+
+  $('#post_attachment').change(function() {
+    readURL(this, preview);
+  });
+
+  $("#owl-demo").owlCarousel({
+
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem : false,
+
+      // "singleItem:true" is a shortcut for:
+       items : 3,
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+
+  });
+});
