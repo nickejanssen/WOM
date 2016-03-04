c2047b3a0c5a891c0a4c557c40cc1d97fd0500f1
nickejanssen <nickejanssen@gmail.com>
2016-03-03 20:49:09 -0500

    Modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-#require 'rubygems'
-require 'aws-sdk'
-
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
