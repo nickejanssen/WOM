241ae4639c024c188b48e56a412166cc0242787d
nickejanssen <nickejanssen@gmail.com>
2016-03-22 17:41:23 -0400

    Commit

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,23 @@
 class MapController < ApplicationController
 	def index
-	end
+    @users = User.all
+    @geojson = Array.new
+
+    @users.each do |user|
+      @geojson << {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [user.longitude, user.latitude]
+        },
+        properties: {
+          name: user.name,
+          address: user.street,
+          :'marker-color' => '#00607d',
+          :'marker-symbol' => 'circle',
+          :'marker-size' => 'medium'
+        }
+      }
+    end
+end
 end
