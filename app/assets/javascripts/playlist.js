4d862ea59d244879b9df79f83b9071b9e2402df0
nickejanssen <nickejanssen@gmail.com>
2016-03-25 17:59:53 -0400

    Added rest of album

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,8 +1,12 @@
 $(function(){
   $('.demoPlate').plate({
     playlist: [
-      {"title":"Raise_Your_Voices","file":"01_Raise_Your_Voices.mp3"},
-      {"title":"Word_of_Mouth","file":"02_Word_of_Mouth.mp3"}
+      {"title":"Raise Your Voices","file":"01_Raise_Your_Voices.mp3"},
+      {"title":"Word of Mouth","file":"02_Word_of_Mouth.mp3"},
+      {"title":"Re-Engage","file":"03_Re-Engage.mp3"},
+      {"title":"This Here Now","file":"04_This_Here_Now.mp3"},
+      {"title":"Tyranny In Harmony","file":"05_Tyranny_In_Harmony.mp3"}
+
     ]
   });
 });
