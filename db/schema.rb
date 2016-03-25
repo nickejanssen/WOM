8f88bb23bce01b55f31ce62700a96bea4f78b5bc
NumberI <x0987654321@ya.ru>
2016-03-25 10:27:09 +0300

    adding longitude to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309005259) do
+ActiveRecord::Schema.define(version: 20160324081746) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -139,6 +139,8 @@ ActiveRecord::Schema.define(version: 20160309005259) do
     t.string   "phone_number"
     t.integer  "posts_count",            default: 0,      null: false
     t.string   "slug"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
