f8021cc76adc9347c46be55e8aa5470dacf0c601
NumberI <x0987654321@ya.ru>
2016-03-25 17:07:32 +0300

    fixing player

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,7 +1,8 @@
 $(function(){
   $('.demoPlate').plate({
     playlist: [
-      {"file":"01_Raise_Your_Voices.mp3"}
+      {"title":"Raise_Your_Voices","file":"01_Raise_Your_Voices.mp3"},
+      {"title":"Word_of_Mouth","file":"02_Word_of_Mouth.mp3"}
     ]
   });
 });
