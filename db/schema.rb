d5d4b46e8b7386c53e56850daf22d2579618a75d
NumberI <x0987654321@ya.ru>
2016-03-31 18:54:40 +0300

    changed tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324081746) do
+ActiveRecord::Schema.define(version: 20160331085830) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -85,6 +85,16 @@ ActiveRecord::Schema.define(version: 20160324081746) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "identities", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "maps", force: true do |t|
     t.datetime "created_at"
