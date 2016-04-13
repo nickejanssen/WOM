959ded1c1d6ea4cc00b1a0485bef6968ebb37fb4
nickejanssen <nickejanssen@gmail.com>
2016-04-13 13:15:16 -0400

    ENV for Trans

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,4 +1,4 @@
-Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.environment = ENV['BRAINTREE_ENV']
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
 Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
 Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
