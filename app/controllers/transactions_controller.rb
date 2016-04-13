ff8b32c2290287f569a03403725dc86ca320ee36
NumberI <x0987654321@ya.ru>
2016-04-13 19:00:42 +0300

    add amount of donation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,15 +4,16 @@ class TransactionsController < ApplicationController
 
   def new
     gon.client_token = generate_client_token
-
   end
 
   def create
     @result = Braintree::Transaction.sale(
-              amount: current_user.cart_total_price,
-              payment_method_nonce: params[:payment_method_nonce])
+      amount: amount,
+      # current_user.cart_total_price,
+      payment_method_nonce: params[:payment_method_nonce])
     if @result.success?
-      current_user.purchase_cart_movies!
+
+      #current_user.purchase_cart_movies!
       redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
@@ -25,6 +26,10 @@ class TransactionsController < ApplicationController
   private
   def generate_client_token
     Braintree::ClientToken.generate
+  end
+
+  def amount
+    params[:donate]
   end
 
 end
