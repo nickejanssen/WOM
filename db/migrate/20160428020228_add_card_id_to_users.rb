990a0e4d66e08ac2ceced3f8e02082768d7dba75
nickejanssen <nickejanssen@gmail.com>
2016-04-27 22:12:03 -0400

    Card ID Field Added

---
+++ db/migrate/20160428020228_add_card_id_to_users.rb
@@ -0,0 +1,5 @@
+class AddCardIdToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :card_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415122027) do
+ActiveRecord::Schema.define(version: 20160428020228) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 20160415122027) do
     t.string   "slug"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "card_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
