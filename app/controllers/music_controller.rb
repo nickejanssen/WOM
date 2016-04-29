9ce3f4e41431b0a41433e5e0974ca84281d5f7bc
NumberI <x0987654321@ya.ru>
2016-04-29 15:35:27 +0300

    added counting of clicked album download

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -6,7 +6,12 @@ class MusicController < ApplicationController
   end
 
   def download
-  	#s3 = Aws::S3::Resource::new(region: ENV['AWS_ALBUM_REGION'])
+  	album_url = "https://www.dropbox.com/s/zoiin6kf29685fk/WOM_SPEAKS.zip?dl=1"
+    @count = Variable.find_by(name: "album_download")
+    down = @count.count + 1
+    @count.update(count: down)
+    redirect_to album_url
+    #s3 = Aws::S3::Resource::new(region: ENV['AWS_ALBUM_REGION'])
   	#url = s3.bucket(ENV['BUCKET_ALBUM']).object('WOM_SPEAKS.zip').presigned_url(:get, expires_in: 1*20.minutes)
   	#redirect_to url
   end
@@ -16,7 +21,6 @@ class MusicController < ApplicationController
   	#url = s3.bucket(ENV['BUCKET_MUSIC']).object('WOM_STEMS.zip').presigned_url(:get, expires_in: 1*20.minutes, response_content_disposition: 'attachment; filename=WOM_STEMS.zip')
   	#redirect_to url
   end
-
 
   def delete
   end
